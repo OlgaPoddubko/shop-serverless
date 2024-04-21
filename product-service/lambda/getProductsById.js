3b4471ef557d3887463be61b61c47edd4289da3c
olga-poddubko <olga.poddubko@xa.epicgames.com>
2024-04-21 20:00:33 +0200

    Task 4.2

--- product-service/lambda/getProductsById.js
+++ product-service/lambda/getProductsById.js
@@ -1,23 +1,52 @@
 'use strict';
 
-const { products } = require('../data');
+const AWS = require('aws-sdk');
 
-const getProductsById = async (event) => {
-    const productId = event.pathParameters?.id;
-    const result = products.find((product) => productId == product.id)
+const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-    if (!result) {
+exports.getProductsById = async (event) => {
+    try {
+        const productId = event.pathParameters.productId;
+
+        const params = {
+            TableName: 'products',
+            Key: {
+                id: productId
+            },
+            ProjectionExpression: 'id, title, description, price, #s.count',
+            ExpressionAttributeNames: { '#s': 'stock' },
+        };
+
+        const data = await dynamodb.get(params).promise();
+
+        if (!data.Item) {
+            return {
+                statusCode: 404,
+                body: JSON.stringify({ message: 'Product not found' }),
+                headers: {
+                    'Access-Control-Allow-Origin' : '*',
+                    'Content-Type': 'application/json',
+
+                },
+            };
+        }
+
         return {
-            statusCode: 404,
-            body: "No such product",
+            statusCode: 200,
+            body: JSON.stringify(data.Item),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        };
+    } catch (error) {
+        console.error('Error fetching product:', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'Internal Server Error' }),
+            headers: {
+                'Access-Control-Allow-Origin' : '*',
+                'Content-Type': 'application/json',
+            },
         };
     }
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify(result),
-        headers:{ 'Access-Control-Allow-Origin' : '*' },
-    };
 };
-
-module.exports = { getProductsById };
--- product-service/lambda/getProductsList.js
+++ product-service/lambda/getProductsList.js
@@ -1,14 +1,38 @@
 'use strict';
 
-const { products } = require('../data');
+const AWS = require('aws-sdk');
 
-const getProductsList = async (event) => {
-    console.log(`products: ${products}`);
-    return {
-        statusCode: 200,
-        body: JSON.stringify(products),
-        headers:{ 'Access-Control-Allow-Origin' : '*' },
-    };
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+exports.getProductsList = async (event) => {
+    try {
+        const params = {
+            TableName: 'products',
+            ProjectionExpression: 'id, title, description, price, #s.count',
+            ExpressionAttributeNames: { '#s': 'stock' },
+            KeyConditionExpression: 'id = #s.product_id',
+        };
+
+        const data = await dynamodb.query(params).promise();
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data.Items),
+            headers: {
+                'Access-Control-Allow-Origin' : '*',
+                'Content-Type': 'application/json',
+            },
+        };
+    } catch (error) {
+        console.error('Error fetching products:', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'Internal Server Error' }),
+            headers: {
+                'Access-Control-Allow-Origin' : '*',
+                'Content-Type': 'application/json',
+            },
+        };
+    }
 };
 
-module.exports = { getProductsList };
